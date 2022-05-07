1aeb48e6f49b2374349a92136ca94d8a0b4b5d07
Sveťa Margetová <SvetlanaM@users.noreply.github.com>
2022-05-07 21:31:26 +0200

    Update set_timestamp.sql

--- set_timestamp.sql
+++ set_timestamp.sql
@@ -10,3 +10,16 @@ BEGIN
   RETURN _new;
 END;
 $function$
+
+CREATE OR REPLACE FUNCTION public.set_current_timestamp_updated_at()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$function$
